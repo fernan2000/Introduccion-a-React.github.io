bda600affbeb330bfe25dd77cf9cb384145ff2a5
vasti <vati050606@gmail.com>
2026-05-20 15:45:52 -0600

    clase 1: tarjetas

--- clase-1/padre_hijo/vite.config.js
+++ clase-1/padre_hijo/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/clase-1/padre_hijo/dist/',
+  base: '/Introduccion-a-React.github.io/clase-1/padre_hijo/dist/',
   plugins: [react()],
 })
--- clase-1/tarjeta/vite.config.js
+++ clase-1/tarjeta/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/clase-1/tarjeta/dist/',
+  base: '/Introduccion-a-React.github.io/clase-1/tarjeta/dist/',
   plugins: [react()],
 })
