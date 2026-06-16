6a045f87738ac4b533723ac7688e88c654dedb2a
vasti <vati050606@gmail.com>
2026-06-16 12:40:26 -0600

    Fix base path clase-6

--- clase-6/gestor-inventario/vite.config.js
+++ clase-6/gestor-inventario/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Introduccion-a-React.github.io/clase-6/gestor-inventario/dist/',
+  base: '/Introduccion-a-React.github.io/clase-6/gestor-inventario/',
 })
