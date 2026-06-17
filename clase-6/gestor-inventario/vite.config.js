7b98c1823002794147e6eec8b1f162f86b019241
vasti <vati050606@gmail.com>
2026-06-16 23:32:59 -0600

    Fix: Corregir base path para GitHub Pages

--- clase-6/gestor-inventario/vite.config.js
+++ clase-6/gestor-inventario/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Introduccion-a-React.github.io/clase-6/gestor-inventario/dist/'
+  base: '/Introduccion-a-React.github.io/clase-6/gestor-inventario/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
